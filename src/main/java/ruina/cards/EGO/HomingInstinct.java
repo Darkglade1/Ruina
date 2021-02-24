75288b9ea086132469255fa6e24873fd271630f2
Darkglade <30379754+Darkglade@users.noreply.github.com>
2021-02-23 19:45:20 -0500

    patch scarier mask and fix homing instinct edge case

--- src/main/java/ruina/cards/EGO/HomingInstinct.java
+++ src/main/java/ruina/cards/EGO/HomingInstinct.java
@@ -56,7 +56,7 @@ public class HomingInstinct extends AbstractEgoCard {
                 }
             }
         };
-        atb(new CallbackExhaustAction(magicNumber, false, false, false, consumer));
+        atb(new CallbackExhaustAction(magicNumber, false, true, true, consumer));
     }
 
     @Override
