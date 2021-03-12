6832ada98e955c4c38803504fb2e421b34637e30
Darkglade <30379754+Darkglade@users.noreply.github.com>
2021-03-11 21:36:26 -0500

    add lamp and beak

---
+++ src/main/java/ruina/cards/EGO/act3/Lamp.java
@@ -0,0 +1,74 @@
+package ruina.cards.EGO.act3;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.ArtifactPower;
+import com.megacrit.cardcrawl.powers.GainStrengthPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import ruina.cards.EGO.AbstractEgoCard;
+import ruina.powers.AbstractLambdaPower;
+
+import static ruina.RuinaMod.makeID;
+import static ruina.util.Wiz.*;
+
+public class Lamp extends AbstractEgoCard {
+    public final static String ID = makeID(Lamp.class.getSimpleName());
+
+    public static final int STRENGTH_LOSS = 4;
+    public static final int UP_STRENGTH_LOSS = 2;
+    public static final int THORNS = 8;
+    public static final int UP_THORNS = 2;
+
+    public static final String POWER_ID = makeID("Lamp");
+    public static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String POWER_NAME = powerStrings.NAME;
+    public static final String[] POWER_DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public Lamp() {
+        super(ID, 1, CardType.SKILL, CardTarget.ENEMY);
+        magicNumber = baseMagicNumber = STRENGTH_LOSS;
+        secondMagicNumber = baseSecondMagicNumber = THORNS;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        atb(new ApplyPowerAction(m, p, new StrengthPower(m, -this.magicNumber), -this.magicNumber));
+        if (m != null && !m.hasPower(ArtifactPower.POWER_ID)) {
+            atb(new ApplyPowerAction(m, p, new GainStrengthPower(m, this.magicNumber), this.magicNumber));
+        }
+        applyToTarget(adp(), adp(), new AbstractLambdaPower(POWER_NAME, POWER_ID, AbstractPower.PowerType.BUFF, false, adp(), secondMagicNumber) {
+            @Override
+            public int onAttacked(DamageInfo info, int damageAmount) {
+                if (info.type != DamageInfo.DamageType.THORNS && info.type != DamageInfo.DamageType.HP_LOSS && info.owner != null && info.owner != this.owner) {
+                    this.flash();
+                    this.addToTop(new DamageAction(info.owner, new DamageInfo(this.owner, this.amount, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL, true));
+                }
+                return damageAmount;
+            }
+
+            @Override
+            public void atStartOfTurn() {
+                atb(new RemoveSpecificPowerAction(adp(), adp(), this));
+            }
+
+            @Override
+            public void updateDescription() {
+                description = POWER_DESCRIPTIONS[0] + amount + POWER_DESCRIPTIONS[1];
+            }
+        });
+    }
+
+    @Override
+    public void upp() {
+        upgradeMagicNumber(UP_STRENGTH_LOSS);
+        upgradeSecondMagic(UP_THORNS);
+    }
+}
