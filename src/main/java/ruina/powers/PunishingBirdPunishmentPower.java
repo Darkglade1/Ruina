84583a7d16d98f812de6ea8a236a81277e66b4c9
Bailey <erringtonbusiness@gmail.com>
2021-03-14 21:53:27 +0000

    QOL: Punishing Bird Icons
    Shows normal when not scheduling an attack, and angry when trigger activated.

--- src/main/java/ruina/powers/PunishingBirdPunishmentPower.java
+++ src/main/java/ruina/powers/PunishingBirdPunishmentPower.java
@@ -1,10 +1,15 @@
 package ruina.powers;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import ruina.RuinaMod;
+import ruina.util.TexLoader;
+
+import static ruina.RuinaMod.makePowerPath;
 
 public class PunishingBirdPunishmentPower extends AbstractEasyPower {
 
@@ -14,8 +19,15 @@ public class PunishingBirdPunishmentPower extends AbstractEasyPower {
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
     private boolean isPunishSet = false;
 
+    private static final Texture passive84 = TexLoader.getTexture(makePowerPath("PBirdPassive84.png"));
+    private static final Texture passive32 = TexLoader.getTexture(makePowerPath("PBirdPassive32.png"));
+    private static final Texture punish84 = TexLoader.getTexture(makePowerPath("PBirdPunish84.png"));
+    private static final Texture punish32 = TexLoader.getTexture(makePowerPath("PBirdPunish32.png"));
+
     public PunishingBirdPunishmentPower(AbstractCreature owner) {
         super(NAME, POWER_ID, PowerType.BUFF, false, owner, -1);
+        setBirdIcon();
+        updateDescription();
     }
 
     public int onAttacked(DamageInfo info, int damageAmount) {
@@ -23,6 +35,7 @@ public class PunishingBirdPunishmentPower extends AbstractEasyPower {
             if (!isPunishSet) {
                 flash();
                 isPunishSet = true;
+                setBirdIcon();
                 updateDescription();
             }
         }
@@ -39,5 +52,17 @@ public class PunishingBirdPunishmentPower extends AbstractEasyPower {
 
     public void setPunishment(boolean v) {
         isPunishSet = v;
+        setBirdIcon();
+    }
+
+    public void setBirdIcon(){
+        if(isPunishSet){
+            this.region128 = new TextureAtlas.AtlasRegion(punish84, 0, 0, 84, 84);
+            this.region48 = new TextureAtlas.AtlasRegion(punish32, 0, 0, 32, 32);
+        }
+        else {
+            this.region128 = new TextureAtlas.AtlasRegion(passive84, 0, 0, 84, 84);
+            this.region48 = new TextureAtlas.AtlasRegion(passive32, 0, 0, 32, 32);
+        }
     }
 }
