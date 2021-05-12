de0df23bc169b75bdb8c4a6b75225c4f9d759f40
Bailey <erringtonbusiness@gmail.com>
2021-05-12 11:19:46 +0100

    better looking cards

--- src/main/java/ruina/cards/ABNO/AbstractAbnormalityCard.java
+++ src/main/java/ruina/cards/ABNO/AbstractAbnormalityCard.java
@@ -24,58 +24,7 @@ import static ruina.RuinaMod.makeImagePath;
 import static ruina.util.Wiz.adp;
 
 public abstract class AbstractAbnormalityCard extends AbstractRuinaCard {
-    public static TextureAtlas.AtlasRegion[] frames;
 
-    public AbstractAbnormalityCard(final String cardID, final int cost, final CardType type, final CardTarget target) {
-        super(cardID, cost, type, CardRarity.RARE, target, RuinaMod.AbnoEnums.ANGELA_LOR_ABNO , makeImagePath("cards/" + CHRALLY_GUN.class.getSimpleName() + ".png"));
-        this.setBannerTexture(RuinaMod.makeImagePath("512/void.png"), RuinaMod.makeImagePath("1024/void.png"));
-        if(frames == null) {
-            frames = new TextureAtlas.AtlasRegion[6];
-            frames[0] = regionFromTexture(RuinaMod.makeImagePath("512/void.png"));
-            frames[1] = regionFromTexture(RuinaMod.makeImagePath("512/void.png"));
-            frames[2] = regionFromTexture(RuinaMod.makeImagePath("512/void.png"));
-            frames[3] = regionFromTexture(RuinaMod.makeImagePath("1024/void.png"));
-            frames[4] = regionFromTexture(RuinaMod.makeImagePath("1024/void.png"));
-            frames[5] = regionFromTexture(RuinaMod.makeImagePath("1024/void.png"));
-        }
-    }
-
-    @SpireOverride
-    protected void renderAttackPortrait(SpriteBatch sb, float x, float y) {
-        try {
-            Method renderHelper = AbstractCard.class.getDeclaredMethod("renderHelper", SpriteBatch.class, Color.class, TextureAtlas.AtlasRegion.class, float.class, float.class);
-            renderHelper.setAccessible(true);
-            renderHelper.invoke(this, sb,
-                    ReflectionHacks.getPrivate(this, AbstractCard.class, "renderColor"),
-                    frames[0], x, y);
-        }catch(Exception ex) {}
-    }
-
-    @SpireOverride
-    protected void renderSkillPortrait(SpriteBatch sb, float x, float y) {
-        try {
-            Method renderHelper = AbstractCard.class.getDeclaredMethod("renderHelper", SpriteBatch.class, Color.class, TextureAtlas.AtlasRegion.class, float.class, float.class);
-            renderHelper.setAccessible(true);
-            renderHelper.invoke(this, sb,
-                    ReflectionHacks.getPrivate(this, AbstractCard.class, "renderColor"),
-                    frames[1], x, y);
-        }catch(Exception ex) {}
-    }
-
-    @SpireOverride
-    protected void renderPowerPortrait(SpriteBatch sb, float x, float y) {
-        try {
-            Method renderHelper = AbstractCard.class.getDeclaredMethod("renderHelper", SpriteBatch.class, Color.class, TextureAtlas.AtlasRegion.class, float.class, float.class);
-            renderHelper.setAccessible(true);
-            renderHelper.invoke(this, sb,
-                    ReflectionHacks.getPrivate(this, AbstractCard.class, "renderColor"),
-                    frames[2], x, y);
-        }catch(Exception ex) {}
-    }
-
-    private TextureAtlas.AtlasRegion regionFromTexture(String tex) {
-        Texture texture = TexLoader.getTexture(tex);
-        return new TextureAtlas.AtlasRegion(texture, 0, 0, texture.getWidth(), texture.getHeight());
-    }
+    public AbstractAbnormalityCard(final String cardID, final int cost, final CardType type, final CardTarget target) { super(cardID, cost, type, CardRarity.RARE, target, RuinaMod.AbnoEnums.ANGELA_LOR_ABNO , makeImagePath("cards/" + CHRALLY_GUN.class.getSimpleName() + ".png")); }
 
 }
