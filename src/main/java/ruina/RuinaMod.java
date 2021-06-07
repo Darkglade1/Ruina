fbe14845cdabd3fff14e5d22b00788a0f290e94d
Bailey <erringtonbusiness@gmail.com>
2021-06-07 03:58:01 +0100

    blood splatter

--- src/main/java/ruina/RuinaMod.java
+++ src/main/java/ruina/RuinaMod.java
@@ -194,6 +194,9 @@ public class RuinaMod implements
     public static String makeCardPath(String resourcePath) {
         return modID + "Resources/images/cards/" + resourcePath;
     }
+    public static String makeVfxPath(String resourcePath) {
+        return modID + "Resources/images/vfx/" + resourcePath;
+    }
 
     public static String makeSFXPath(String resourcePath) {
         return modID + "Resources/audio/sfx/" + resourcePath;
