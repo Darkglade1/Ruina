9b32e06849136780cd704eee1b524f870ca5bb4f
Darkglade <30379754+Darkglade@users.noreply.github.com>
2021-07-03 15:24:38 -0400

    rework wingbeat

--- src/main/java/ruina/cards/EGO/act1/Wingbeat.java
+++ src/main/java/ruina/cards/EGO/act1/Wingbeat.java
@@ -1,39 +1,70 @@
 package ruina.cards.EGO.act1;
 
-import com.evacipated.cardcrawl.mod.stslib.variables.ExhaustiveVariable;
-import com.megacrit.cardcrawl.actions.common.DiscardAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.DexterityPower;
+import com.megacrit.cardcrawl.relics.ChemicalX;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+import ruina.actions.CallbackExhaustAction;
 import ruina.cards.EGO.AbstractEgoCard;
 
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
 import static ruina.RuinaMod.makeID;
-import static ruina.util.Wiz.applyToTarget;
-import static ruina.util.Wiz.atb;
+import static ruina.util.Wiz.*;
 
 public class Wingbeat extends AbstractEgoCard {
     public final static String ID = makeID(Wingbeat.class.getSimpleName());
 
-    public static final int DEX = 2;
-    public static final int UP_DEX = 1;
-    public static final int DISCARD = 2;
-    public static final int EXHAUSTIVE = 3;
-
     public Wingbeat() {
-        super(ID, 1, CardType.SKILL, CardTarget.SELF);
-        magicNumber = baseMagicNumber = DEX;
-        secondMagicNumber = baseSecondMagicNumber = DISCARD;
-        ExhaustiveVariable.setBaseValue(this, EXHAUSTIVE);
+        super(ID, -1, CardType.SKILL, CardTarget.SELF);
+        exhaust = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        atb(new DiscardAction(p, p, secondMagicNumber, false));
-        applyToTarget(p, p, new DexterityPower(p, magicNumber));
+        atb(new AbstractGameAction() {
+            @Override
+            public void update() {
+                int effect = EnergyPanel.totalCount;
+                if (energyOnUse != -1) {
+                    effect = energyOnUse;
+                }
+
+                if (adp().hasRelic(ChemicalX.ID)) {
+                    effect += 2;
+                    adp().getRelic(ChemicalX.ID).flash();
+                }
+
+                if (effect > 0) {
+                    Consumer<ArrayList<AbstractCard>> consumer = abstractCards -> {
+                        int totalCost = 0;
+                        for (AbstractCard card : abstractCards) {
+                            if (card.costForTurn >= 0) {
+                                totalCost += card.costForTurn;
+                            }
+                        }
+                        if (totalCost > 0) {
+                            atb(new HealAction(p, p, totalCost));
+                            applyToTarget(p, p, new DexterityPower(p, totalCost));
+                        }
+                    };
+                    att(new CallbackExhaustAction(effect, false, true, true, consumer));
+                    if (!freeToPlayOnce) {
+                        adp().energy.use(EnergyPanel.totalCount);
+                    }
+                }
+                this.isDone = true;
+            }
+        });
     }
 
     @Override
     public void upp() {
-        upgradeMagicNumber(UP_DEX);
+        selfRetain = true;
     }
 }
