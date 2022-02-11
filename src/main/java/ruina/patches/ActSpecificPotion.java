a244649a1a3322c5c74b997c3601b33bd7e5f9e1
Darkglade <30379754+Darkglade@users.noreply.github.com>
2022-02-11 16:45:11 -0500

    make ego potion act specific

---
+++ src/main/java/ruina/patches/ActSpecificPotion.java
@@ -0,0 +1,52 @@
+package ruina.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.PotionHelper;
+import com.megacrit.cardcrawl.potions.AbstractPotion;
+import com.megacrit.cardcrawl.potions.DuplicationPotion;
+import com.megacrit.cardcrawl.random.Random;
+import ruina.dungeons.AbstractRuinaDungeon;
+import ruina.potions.EgoPotion;
+
+public class ActSpecificPotion {
+
+    @SpirePatch(
+            clz = PotionHelper.class,
+            method = "getRandomPotion",
+            paramtypez = {
+                    Random.class
+            }
+    )
+    public static class patch1 {
+        @SpirePostfixPatch
+        public static AbstractPotion Postfix(AbstractPotion original, Random rng) {
+            if (AbstractDungeon.player != null && original instanceof EgoPotion && !(CardCrawlGame.dungeon instanceof AbstractRuinaDungeon)) {
+                System.out.println("Preventing Ruina potion from spawning in non-Ruina Act, spawning Dupe potion instead");
+                return new DuplicationPotion();
+            } else {
+                return original;
+            }
+        }
+    }
+
+    @SpirePatch(
+            clz = PotionHelper.class,
+            method = "getRandomPotion",
+            paramtypez = {}
+    )
+    public static class patch2 {
+        @SpirePostfixPatch
+        public static AbstractPotion Postfix(AbstractPotion original) {
+            if (AbstractDungeon.player != null && original instanceof EgoPotion && !(CardCrawlGame.dungeon instanceof AbstractRuinaDungeon)) {
+                System.out.println("Preventing Ruina potion from spawning in non-Ruina Act, spawning Dupe potion instead");
+                return new DuplicationPotion();
+            } else {
+                return original;
+            }
+        }
+    }
+
+}
