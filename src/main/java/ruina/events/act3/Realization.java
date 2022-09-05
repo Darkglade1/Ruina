471328c1d45b29e4c1eab33bf5ca5f404baba219
Darkglade1 <jeffrey.zhang.ga@gmail.com>
2022-09-05 19:23:39 -0400

    wip

--- src/main/java/ruina/events/act3/Realization.java
+++ src/main/java/ruina/events/act3/Realization.java
@@ -14,6 +14,10 @@ import com.megacrit.cardcrawl.localization.EventStrings;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import ruina.RuinaMod;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 import static ruina.RuinaMod.makeEventPath;
 
 public class Realization extends AbstractEvent {
@@ -26,16 +30,66 @@ public class Realization extends AbstractEvent {
     private static final String[] OPTIONS = eventStrings.OPTIONS;
     public static final String IMG = makeEventPath("Realization.png");
 
-    private static final int CARD_REMOVAL = 2;
-    private static final int NUM_RELICS = 2;
+    private enum AbnoStory {
+        GREED(1), DESPAIR(7), HATE(13);
+
+        private int start;
+        ArrayList<String> story1;
+        ArrayList<String> story2;
+
+        AbnoStory(int start) {
+            this.start = start;
+            story1 = new ArrayList<>();
+            story1.add(DESCRIPTIONS[start + 1]);
+            story1.add(DESCRIPTIONS[start + 2]);
+            story1.add(DESCRIPTIONS[start + 3]);
+            story2 = new ArrayList<>();
+            story2.add(DESCRIPTIONS[start + 4]);
+            story2.add(DESCRIPTIONS[start + 5]);
+        }
+    }
+
+    private enum AbnoOptions {
+        GREED(0), DESPAIR(5), HATE(10);
+
+        private int start;
+        ArrayList<String> option1;
+        ArrayList<String> option2;
+
+        AbnoOptions(int start) {
+            this.start = start;
+            option1 = new ArrayList<>();
+            option1.add(DESCRIPTIONS[start + 1]);
+            option1.add(DESCRIPTIONS[start + 2]);
+            option2 = new ArrayList<>();
+            option2.add(DESCRIPTIONS[start + 3]);
+            option2.add(DESCRIPTIONS[start + 4]);
+        }
+    }
+
     AbstractCard curse = CardLibrary.getCopy(Shame.ID);
 
     private int screenNum = 0;
+    private AbnoStory abnoStory;
+    private AbnoOptions abnoOptions;
+
+    private AbnoOptions option1;
+    private AbnoOptions option2;
 
     public Realization() {
         this.body = DESCRIPTIONS[0];
-        this.roomEventText.addDialogOption(OPTIONS[0]);
-        this.roomEventText.addDialogOption(OPTIONS[1]);
+        AbnoOptions greed = AbnoOptions.GREED;
+        AbnoOptions despair = AbnoOptions.DESPAIR;
+        AbnoOptions hate = AbnoOptions.HATE;
+        ArrayList<AbnoOptions> shuffle = new ArrayList<>();
+        shuffle.add(greed);
+        shuffle.add(despair);
+        shuffle.add(hate);
+        Collections.shuffle(shuffle, AbstractDungeon.eventRng.random);
+        option1 = shuffle.get(0);
+        option2 = shuffle.get(1);
+        this.roomEventText.addDialogOption(OPTIONS[option1.start]);
+        this.roomEventText.addDialogOption(OPTIONS[option2.start]);
         AbstractDungeon.getCurrRoom().phase = AbstractRoom.RoomPhase.EVENT;
         this.hasDialog = true;
         this.hasFocus = true;
@@ -61,13 +115,30 @@ public class Realization extends AbstractEvent {
             case 0:
                 switch (buttonPressed) {
                     case 0:
+                        abnoOptions = option1;
+                        abnoStory = getStoryFromOption(option1);
                         break;
                     case 1:
+                        abnoOptions = option2;
+                        abnoStory = getStoryFromOption(option2);
                         break;
                 }
+                this.roomEventText.updateBodyText(DESCRIPTIONS[abnoStory.start]);
+                screenNum = 1;
                 break;
             default:
                 this.openMap();
+        }
+    }
+
+    private AbnoStory getStoryFromOption(AbnoOptions option) {
+        if (option == AbnoOptions.GREED) {
+            return AbnoStory.GREED;
+        }
+        if (option == AbnoOptions.DESPAIR) {
+            return AbnoStory.DESPAIR;
+        } else {
+            return AbnoStory.HATE;
         }
     }
 }
