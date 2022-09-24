44fc81700a121f1c07af10ab620375c7941c7b37
Darkglade1 <jeffrey.zhang.ga@gmail.com>
2022-09-24 13:59:33 -0400

    silent girl now phases like awakened one

--- src/main/java/ruina/monsters/act3/silentGirl/SilentGirl.java
+++ src/main/java/ruina/monsters/act3/silentGirl/SilentGirl.java
@@ -4,9 +4,7 @@ import actlikeit.dungeons.CustomDungeon;
 import basemod.helpers.CardPowerTip;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.HealAction;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
-import com.megacrit.cardcrawl.actions.common.RollMoveAction;
+import com.megacrit.cardcrawl.actions.common.*;
 import com.megacrit.cardcrawl.actions.unique.RemoveDebuffsAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
@@ -16,17 +14,19 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.localization.MonsterStrings;
 import com.megacrit.cardcrawl.localization.PowerStrings;
-import com.megacrit.cardcrawl.powers.AbstractPower;
-import com.megacrit.cardcrawl.powers.FrailPower;
-import com.megacrit.cardcrawl.powers.StrengthPower;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.*;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
 import ruina.BetterSpriterAnimation;
 import ruina.cards.Guilt;
 import ruina.monsters.AbstractRuinaMonster;
 import ruina.powers.AbstractLambdaPower;
+import ruina.powers.CenterOfAttention;
+import ruina.powers.NextTurnPowerPower;
 import ruina.powers.Paralysis;
 import ruina.vfx.VFXActionButItCanFizzle;
 import ruina.vfx.WaitEffect;
+
+import java.util.ArrayList;
 
 import static ruina.RuinaMod.makeID;
 import static ruina.RuinaMod.makeMonsterPath;
@@ -56,6 +56,7 @@ public class SilentGirl extends AbstractRuinaMonster
     private final int CURSE_AMT = 1;
     private final float HP_THRESHOLD = 0.5f;
     private int enraged = 1; //1 is false, 2 is true
+    private final int maxHP = calcAscensionTankiness(240);
 
     private DummyHammer hammer = new DummyHammer(100.0f, 0.0f);
     private DummyNail nail = new DummyNail(-300.0f, 0.0f);
@@ -74,14 +75,14 @@ public class SilentGirl extends AbstractRuinaMonster
         super(NAME, ID, 480, 0.0F, 0, 250.0f, 290.0f, null, x, y);
         this.animation = new BetterSpriterAnimation(makeMonsterPath("SilentGirl/Spriter/SilentGirl.scml"));
         this.type = EnemyType.BOSS;
-        setHp(calcAscensionTankiness(480));
+        setHp(maxHP);
         addMove(DIGGING_NAIL, Intent.ATTACK_DEBUFF, calcAscensionDamage(16));
         addMove(SLAM, Intent.ATTACK_DEFEND, calcAscensionDamage(20));
         addMove(A_CRACKED_HEART, Intent.ATTACK_DEBUFF, calcAscensionDamage(15));
         addMove(COLLAPSING_HEART, Intent.ATTACK, calcAscensionDamage(22));
         addMove(BROKEN, Intent.ATTACK, calcAscensionDamage(12), 2, true);
         addMove(LEER, Intent.DEBUFF);
-        addMove(SUPPRESS, Intent.DEFEND_BUFF);
+        addMove(SUPPRESS, Intent.UNKNOWN);
 
         if (AbstractDungeon.ascensionLevel >= 19) {
             curse.upgrade();
@@ -108,6 +109,8 @@ public class SilentGirl extends AbstractRuinaMonster
                 description = POWER_DESCRIPTIONS[0] + CURSE_AMT + POWER_DESCRIPTIONS[1] + FontHelper.colorString(curse.name, "y") + POWER_DESCRIPTIONS[2];
             }
         });
+        AbstractDungeon.getCurrRoom().cannotLose = true;
+        applyToTarget(this, this, new UnawakenedPower(this));
     }
 
     @Override
@@ -170,13 +173,12 @@ public class SilentGirl extends AbstractRuinaMonster
                 phaseChangeAnimation();
                 hammer.deadAnimation();
                 nail.deadAnimation();
-                atb(new RemoveDebuffsAction(this));
+                this.halfDead = false;
+                atb(new HealAction(this, this, maxHealth));
                 block(this, BLOCK * 3);
-                if (AbstractDungeon.ascensionLevel >= 19) {
-                    atb(new HealAction(this, this, (int)(maxHealth * 0.25f)));
-                }
                 applyToTarget(this, this, new StrengthPower(this, STRENGTH));
                 enraged = 2;
+                AbstractDungeon.getCurrRoom().cannotLose = false;
                 resetIdle(1.0f);
                 break;
             }
@@ -187,9 +189,7 @@ public class SilentGirl extends AbstractRuinaMonster
     @Override
     protected void getMove(final int num) {
         if (enraged == 1) {
-            if (currentHealth <= maxHealth * HP_THRESHOLD) {
-                setMoveShortcut(SUPPRESS, MOVES[SUPPRESS]);
-            } else if (lastMove(DIGGING_NAIL)) {
+            if (lastMove(DIGGING_NAIL)) {
                 setMoveShortcut(SLAM, MOVES[SLAM]);
             } else if (lastMove(SLAM)) {
                 setMoveShortcut(A_CRACKED_HEART, MOVES[A_CRACKED_HEART]);
@@ -222,10 +222,8 @@ public class SilentGirl extends AbstractRuinaMonster
     @Override
     public void render(SpriteBatch sb) {
         super.render(sb);
-        if (!this.isDeadOrEscaped()) {
-            hammer.render(sb);
-            nail.render(sb);
-        }
+        hammer.render(sb);
+        nail.render(sb);
     }
 
     @Override
@@ -235,9 +233,48 @@ public class SilentGirl extends AbstractRuinaMonster
     }
 
     @Override
+    public void damage(DamageInfo info) {
+        super.damage(info);
+        if (this.currentHealth <= 0 && !this.halfDead && AbstractDungeon.getCurrRoom().cannotLose) {
+            this.halfDead = true;
+            for (AbstractPower p : this.powers) {
+                p.onDeath();
+            }
+            for (AbstractRelic r : AbstractDungeon.player.relics) {
+                r.onMonsterDeath(this);
+            }
+            if (this.nextMove != SUPPRESS) {
+                setMoveShortcut(SUPPRESS);
+                this.createIntent();
+                atb(new SetMoveAction(this, SUPPRESS, Intent.NONE));
+            }
+            ArrayList<AbstractPower> powersToRemove = new ArrayList<>();
+            for (AbstractPower power : this.powers) {
+                if (!(power.ID.equals(POWER_ID)) && !(power.ID.equals(StrengthPower.POWER_ID)) && !(power instanceof NextTurnPowerPower)) {
+                    powersToRemove.add(power);
+                }
+            }
+            for (AbstractPower power : powersToRemove) {
+                this.powers.remove(power);
+            }
+        }
+    }
+
+    @Override
     public void die(boolean triggerRelics) {
-        super.die(triggerRelics);
-        onBossVictoryLogic();
+        if (!AbstractDungeon.getCurrRoom().cannotLose) {
+            super.die(triggerRelics);
+            onBossVictoryLogic();
+        }
+        if (this.maxHealth <= 0) {
+            setMaxHP();
+            AbstractDungeon.actionManager.addToBottom(new InstantKillAction(this));
+        }
+    }
+
+    private void setMaxHP() {
+        this.maxHealth = maxHP;
+        healthBarUpdatedEvent();
     }
 
     private void rangedAnimation(AbstractCreature enemy) {
