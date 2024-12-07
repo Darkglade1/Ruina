eea7053dd205811a52ce61386879120f465bfdd6
Darkglade1 <jeffrey.zhang.ga@gmail.com>
2024-12-07 16:42:40 -0500

    make ally tutorial nicer

--- src/main/java/ruina/tutorials/AllyTutorial.java
+++ src/main/java/ruina/tutorials/AllyTutorial.java
@@ -1,22 +1,25 @@
 package ruina.tutorials;
 
+import basemod.abstracts.CustomMultiPageFtue;
+import com.badlogic.gdx.graphics.Texture;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.TutorialStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.ui.FtueTip;
 import ruina.RuinaMod;
 import ruina.monsters.AbstractAllyMonster;
+import ruina.util.TexLoader;
 
 import java.io.IOException;
 
-public class AllyTutorial extends FtueTip {
+import static ruina.RuinaMod.makeUIPath;
 
-    public AllyTutorial(String label, String text, float x, float y, FtueTip.TipType type) {
-        super(label, text, x, y, type);
+public class AllyTutorial extends CustomMultiPageFtue {
+
+    public AllyTutorial(Texture[] images, String[] messages) {
+        super(images, messages);
     }
 
     @SpirePatch(
@@ -26,14 +29,16 @@ public class AllyTutorial extends FtueTip {
     public static class ShowAllyTutorialPatch {
         public static final String ID = RuinaMod.makeID("AllyTutorial");
         public static final TutorialStrings tutorialStrings = CardCrawlGame.languagePack.getTutorialString(ID);
-        public static final String[] LABEL = tutorialStrings.LABEL;
         public static final String[] TEXT = tutorialStrings.TEXT;
 
         public static void Postfix(DrawCardAction __instance) {
             if (__instance.isDone && !RuinaMod.ruinaConfig.getBool("Ally Tutorial Seen")) {
                 for (AbstractMonster mo : AbstractDungeon.getMonsters().monsters) {
                     if (mo instanceof AbstractAllyMonster) {
-                        AbstractDungeon.ftue = new AllyTutorial(LABEL[0], TEXT[0], Settings.WIDTH / 2.0f - (500.0f * Settings.scale), Settings.HEIGHT / 2.0f, TipType.COMBAT);
+                        Texture tip1 = TexLoader.getTexture(makeUIPath("AllyTutorial.png"));
+                        Texture[] images = new Texture[1];
+                        images[0] = tip1;
+                        AbstractDungeon.ftue = new AllyTutorial(images, TEXT);
                         RuinaMod.ruinaConfig.setBool("Ally Tutorial Seen", true);
                         try { RuinaMod.ruinaConfig.save(); } catch (IOException e) { e.printStackTrace(); }
                         break;
