faf2a5f416a30b11876e59585ef7162ddc58f567
Darkglade1 <jeffrey.zhang.ga@gmail.com>
2024-12-07 16:27:11 -0500

    make ally buttons nicer

--- src/main/java/ruina/util/AllyMove.java
+++ src/main/java/ruina/util/AllyMove.java
@@ -5,26 +5,25 @@ import basemod.ClickableUIElement;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.helpers.Hitbox;
 import com.megacrit.cardcrawl.helpers.TipHelper;
+import com.megacrit.cardcrawl.helpers.controller.CInputActionSet;
+import com.megacrit.cardcrawl.helpers.input.InputHelper;
 import ruina.monsters.AbstractAllyMonster;
 
 import static ruina.util.Wiz.adp;
 
-
 public class AllyMove extends ClickableUIElement {
 
-    private String ID;
-    private String moveDescription;
-    private Texture moveImage;
-    private Runnable moveActions;
-    private AbstractAllyMonster owner;
+    private final String ID;
+    private final String moveDescription;
+    private final Runnable moveActions;
+    private final AbstractAllyMonster owner;
 
     public AllyMove(String ID, AbstractAllyMonster owner, Texture moveImage, String moveDescription, Runnable moveActions) {
         super(moveImage, 0, 0, 64.0f, 64.0f);
-        this.moveImage = moveImage;
         this.moveActions = moveActions;
         this.ID = ID;
         this.moveDescription = moveDescription;
@@ -35,33 +34,20 @@ public class AllyMove extends ClickableUIElement {
         if(moveActions != null) {
             moveActions.run();
         } else {
-            BaseMod.logger.info("MinionMove: " + this.ID + " had no actions!");
+            BaseMod.logger.info("AllyMove: " + this.ID + " had no actions!");
         }
-    }
-
-    public Hitbox getHitbox() {
-        return this.hitbox;
-    }
-
-    public Texture getMoveImage(){
-        return this.moveImage;
-    }
-
-    public void setMoveImage(Texture moveImage){
-        this.moveImage = moveImage;
     }
 
     public String getID(){
         return this.ID;
     }
 
-    public String getMoveDescription(){
-        return this.moveDescription;
-    }
-
     @Override
     protected void onHover() {
         TipHelper.renderGenericTip(this.x, this.y - 15f * Settings.scale, this.ID, this.moveDescription);
+        if (this.hitbox.justHovered && canUse()) {
+            CardCrawlGame.sound.playV("UI_HOVER", 0.75F);
+        }
     }
 
     @Override
@@ -69,17 +55,26 @@ public class AllyMove extends ClickableUIElement {
 
     }
 
+    private boolean canUse() {
+        return !AbstractDungeon.actionManager.turnHasEnded && !adp().inSingleTargetMode && !adp().isDraggingCard && !AbstractDungeon.isScreenUp;
+    }
+
     @Override
     protected void onClick() {
-        if(!AbstractDungeon.actionManager.turnHasEnded && !adp().inSingleTargetMode && !adp().isDraggingCard){
+        if(canUse()){
+            InputHelper.justClickedLeft = false;
+            CInputActionSet.select.unpress();
+            CardCrawlGame.sound.play("UI_CLICK_1");
             this.doMove();
         }
     }
 
     @Override
     public void render(SpriteBatch sb) {
-        if(AbstractDungeon.actionManager.turnHasEnded){
+        if(!canUse()){
             super.render(sb, Color.GRAY);
+        } else if (this.hitbox.hovered) {
+            super.render(sb, Color.GOLD);
         } else {
             super.render(sb);
         }
