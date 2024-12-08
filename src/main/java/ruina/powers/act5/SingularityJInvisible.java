74c44a32d8d52c47f11b395ab6e02b161076ab16
Darkglade1 <jeffrey.zhang.ga@gmail.com>
2024-12-08 15:20:36 -0500

    add file

---
+++ src/main/java/ruina/powers/act5/SingularityJInvisible.java
@@ -0,0 +1,12 @@
+package ruina.powers.act5;
+
+import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.InvisiblePower;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class SingularityJInvisible extends SingularityJ implements InvisiblePower {
+
+    public SingularityJInvisible(AbstractCreature owner, AbstractMonster otherEnemy) {
+        super(owner, otherEnemy);
+    }
+}
